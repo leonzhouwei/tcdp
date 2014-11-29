49f3e07617fd7c09470e01568cbdc63624b39c39
zhouwei <leonzhouwei@gmail.com>
2014-11-29 12:44:46 +0800

    web ui works

--- tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/controller/CalculationController.java
+++ tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/controller/CalculationController.java
@@ -3,19 +3,18 @@ package com.example.trafficcarddataprocess.calculator.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/calculation")
 public class CalculationController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CalculationController.class);
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-    public ModelAndView echo(@PathVariable("taskId") String taskId, @PathVariable("roadId") String roadId) {  
+	@RequestMapping(value="/calculation", method=RequestMethod.POST)
+    public ModelAndView echo(@RequestParam("taskId") String taskId, @RequestParam("roadId") String roadId) {  
 		logger.debug("task id: " + taskId);
 		logger.debug("road id: " + roadId);
         ModelAndView mav = new ModelAndView();  
--- tcdp-calculator/src/test/java/com/example/trafficcarddataprocess/calculator/AppTest.java
+++ tcdp-calculator/src/test/java/com/example/trafficcarddataprocess/calculator/AppTest.java
@@ -3,9 +3,7 @@ package com.example.trafficcarddataprocess.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class AppTest {
