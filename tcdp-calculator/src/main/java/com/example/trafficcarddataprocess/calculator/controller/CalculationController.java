5a57e6f0bcbfacb41d6d626cfa14ef363c97c8f4
zhouwei <leonzhouwei@gmail.com>
2014-11-30 20:04:54 +0800

    test

--- tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/controller/CalculationController.java
+++ tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/controller/CalculationController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,35 @@ public class CalculationController {
 	
 	@RequestMapping(value="/calculation", method=RequestMethod.GET)
     public ModelAndView echo(@RequestParam("taskId") String taskIdParam, @RequestParam("roadId") String roadIdParam) {  
+		logger.debug("task id: " + taskIdParam);
+		logger.debug("road id: " + roadIdParam);
+		List<Result> list = Lists.newArrayList();
+		for (int i = 1; i < 11; ++i) {
+			Result e = new Result();
+			long id = i;
+			long taskId = i;
+			long roadId = i;
+			double speed = i;
+			double speedConfi = i;
+			long flow = i;
+			double flowConfi = i;
+			e.setId(id);
+			e.setTaskId(taskId);
+			e.setRoadSectionId(roadId);
+			e.setAverageSpeed(speed);
+			e.setAverageSpeedConfidence(speedConfi);
+			e.setTrafficFlow(flow);
+			e.setTrafficFlowConfidence(flowConfi);
+			list.add(e);
+		}
+		Map<String, Object> model = Maps.newHashMap();
+		model.put("results", list);
+        ModelAndView mav = new ModelAndView("calculation_result", model);  
+        return mav;  
+    }
+	
+	@RequestMapping(value="/calculations/tasks/{taskId}/roads/{roadId}", method=RequestMethod.GET)
+    public ModelAndView get(@PathVariable("taskId") String taskIdParam, @PathVariable("roadId") String roadIdParam) {  
 		logger.debug("task id: " + taskIdParam);
 		logger.debug("road id: " + roadIdParam);
 		List<Result> list = Lists.newArrayList();
