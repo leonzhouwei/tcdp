3ffc6c0f5ca932d8082517906653c6929fa40ca0
wei.zhou <wei.zhou@autonavi.com>
2014-12-01 17:30:23 +0800

    host, port and session timemout can be configured via modifying the configuration file(confin/application.properties)

--- tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/App.java
+++ tcdp-calculator/src/main/java/com/example/trafficcarddataprocess/calculator/App.java
@@ -1,7 +1,13 @@
 package com.example.trafficcarddataprocess.calculator;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -10,8 +16,21 @@ import org.springframework.context.annotation.ImportResource;
 @EnableAutoConfiguration
 public class App {
 	
+	@Value("${server.port}")
+	private int port;
+	@Value("${server.sessionTimeout}")
+	private int sessionTimeout;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+	    factory.setPort(port);
+	    factory.setSessionTimeout(sessionTimeout, TimeUnit.SECONDS);
+	    return factory;
 	}
 
 }
