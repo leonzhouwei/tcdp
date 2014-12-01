fc882e53f6c8abc912ffa1622651aedcc45ac38f
wei.zhou <wei.zhou@autonavi.com>
2014-12-01 16:12:21 +0800

    delete unused projects

--- tcdp-webapi/src/main/java/com/example/trafficcarddataprocess/webapi/domain/Road.java
+++
@@ -1,5 +0,0 @@
-package com.example.trafficcarddataprocess.webapi.domain;
-
-public class Road {
-
-}
--- tcdp-webapi/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-base_path=$(cd `dirname $0`; pwd)
-cd $base_path
-
-java -jar tcdp-webapi*.jar
-
-exit 0
-
