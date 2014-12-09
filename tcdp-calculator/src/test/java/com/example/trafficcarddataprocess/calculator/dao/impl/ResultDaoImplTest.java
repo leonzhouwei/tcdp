239b7bd45487270c2653952113a88b135d8da641
wei.zhou <wei.zhou@autonavi.com>
2014-12-09 17:49:51 +0800

    add support for Declarative Transaction Management

---
+++ tcdp-calculator/src/test/java/com/example/trafficcarddataprocess/calculator/dao/impl/ResultDaoImplTest.java
@@ -0,0 +1,32 @@
+package com.example.trafficcarddataprocess.calculator.dao.impl;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.trafficcarddataprocess.calculator.App;
+import com.example.trafficcarddataprocess.calculator.dao.ResultDao;
+import com.example.trafficcarddataprocess.calculator.domain.Result;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+public class ResultDaoImplTest {
+	
+	@Autowired
+	private ResultDao dao;
+
+	public void testSave() {
+		Result result = new Result();
+		result.setTaskId(1L);
+		result.setRoadSectionId(1L);
+		result.setTravelTime(1L);
+		result.setAverageSpeed(1.1);
+		result.setAverageSpeedConfidence(0.1);
+		result.setTrafficFlow(1L);
+		result.setTrafficFlowConfidence(0.1);
+		dao.save(result);
+	}
+
+
+}
